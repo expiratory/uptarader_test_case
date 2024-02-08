16d8e05e7133bd92c3df9d99e46495381ff8c79d
Alexandr Kilchevskiy <alexkill94@mail.ru>
2024-02-08 09:01:25 +0300

    lgtm

--- abstract_cms/menu/templatetags/custom_tags.py
+++ abstract_cms/menu/templatetags/custom_tags.py
@@ -5,15 +5,50 @@ from ..models import MenuItem
 register = template.Library()
 
 
-@register.inclusion_tag('menu.html')
-def draw_menu(menu_name, children=None):
-    if children is None and menu_name is not None:
-        menu_items = MenuItem.objects.filter(parent__isnull=True, menu_name=menu_name)
-        return {'menu_items': menu_items}
-    else:
-        return {'menu_items': children}
+@register.inclusion_tag('menu.html', takes_context=True)
+def draw_menu(context, menu_name):
+    current_url = context['current_url']
+
+    menu_items = MenuItem.objects.filter(menu_name=menu_name).values()
+    root_items = [item for item in menu_items.filter(parent=None)]
+    current_item = menu_items.filter(url__in=current_url).first()
+
+    if current_item is None:
+        return {'menu_items': root_items}
+
+    ancestor_items = get_ancestors(current_item, root_items, menu_items)
+
+    for item in root_items:
+        if item in ancestor_items:
+            item['children'] = get_descendants(menu_items, item, ancestor_items)
+    return {'menu_items': root_items}
 
 
-@register.filter
-def has_children(item):
-    return item.children.exists()
+def get_ancestors(current_item, root_items, menu_items):
+    ancestors_items = []
+
+    while current_item:
+        ancestors_items.append(current_item)
+        current_item = menu_items.get(id=current_item['parent_id']) if current_item['parent_id'] else None
+
+    if ancestors_items:
+        return ancestors_items
+
+    for item in root_items:
+        if item.id == current_item.id:
+            ancestors_items.append(item)
+    return ancestors_items
+
+
+def get_descendants(menu_items, item, ancestor_items):
+    descendants_items = [item for item in menu_items.filter(parent=item['id'])]
+
+    for item in descendants_items:
+        if item in ancestor_items:
+            item['children'] = get_descendants(menu_items, item, ancestor_items)
+    return descendants_items
+
+
+@register.filter(name='startswith')
+def startswith(value, arg):
+    return value.startswith(arg)
--- abstract_cms/menu/views.py
+++ abstract_cms/menu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.urls import resolve
 
 
 def menu_view(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', {'current_url': [request.path, resolve(request.path).url_name]})
